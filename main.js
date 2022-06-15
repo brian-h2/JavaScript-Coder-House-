556a46c2a17c6443c74f28a6078be4a71997ae59
Brian Heredia <herediabrian123@gmail.com>
2022-06-15 17:25:59 -0300

    Agregado de dos ejercicios mas

--- main.js
+++ main.js
@@ -7,3 +7,24 @@ for (let i = 1; i <= 10; i++) {
     alert(numero + " " + "+" + " " + i + " " + "="  + " " + resultado )
    
 }
+
+/*
+let texto = ""
+
+do {
+    texto = prompt("INGRESE UN TEXTO")
+    alert("El texto es " + texto)
+}while (texto != "ESC") {
+    alert("El usuario ingreso " + texto + " entonces se procede a terminar el ciclo")
+}
+
+let numero = prompt("INGRESE UN NUMERO")
+let palabra = "hola"
+for (let i= 0; i < numero; i++) {
+    let numero = palabra
+    alert(palabra)
+}*/
+
+
+
+
