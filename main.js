098aea5722b85519bd22fd1f77adc2732f7d502e
Brian Heredia <herediabrian123@gmail.com>
2022-06-15 16:59:19 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,9 @@
+//Desafio complementario 1
+
+let numero = parseInt(prompt("INGRESE UN NUMERO")) 
+
+for (let i = 1; i <= 10; i++) {
+    let resultado = numero + i
+    alert(numero + " " + "+" + " " + i + " " + "="  + " " + resultado )
+   
+}
